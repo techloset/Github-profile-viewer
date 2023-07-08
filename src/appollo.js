5086cdba81e05c58cf68a726d5a82417089cd594
Tayyab Ali <TayyabAli204>
2023-07-08 22:46:52 +0500

    done

--- src/appollo.js
+++ src/appollo.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 const client = new ApolloClient({
-  uri: 'https://api.github.com/graphql',
+  uri: 'https://api.github.com/graphql/user',
   cache: new InMemoryCache(),
   headers: {
     Authorization: 'Bearer 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',
